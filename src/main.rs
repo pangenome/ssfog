31efe7fd39db3a8a32a7c9ff77217a252f02b2de
Erik Garrison <erik.garrison@gmail.com>
2021-05-18 14:06:12 +0200

    derivatives that we should probably smooth

--- src/main.rs
+++ src/main.rs
@@ -46,6 +46,14 @@ fn normal_pdf(mu : f64 , sigma : f64, x : f64)-> f64{
     E.powf(-1.0/2.0 * (((x-mu)/sigma).powf(2.0)) / (sigma * (2.0 * PI).sqrt() ))
 }
 
+fn differentiate(v: &[f64]) -> Vec<f64> {
+    let mut p = &v[0];
+    v[1..].iter().map(|c| {
+        let d = c - p;
+        p = c;
+        d
+    }).collect::<Vec<f64>>()
+}
 
 fn main() {
     let matches = App::new("ssfog")
@@ -66,26 +74,45 @@ fn main() {
                 .takes_value(true)
                 .help("sigma for our gaussian"),
         )
+        .arg(
+            Arg::with_name("nth-derivative")
+                .short("d")
+                .long("nth-derivative")
+                .takes_value(true)
+                .help("return the nth derivative of the signal"))
         .get_matches();
 
     let sigma = matches
         .value_of("sigma")
         .unwrap_or(&"1")
         .parse::<usize>()
-        .unwrap();    // let dist  = Normal::new(0.0, 100.0);
-    let impulse_len = 6*sigma;
+        .unwrap();
+    let nth_derivative = matches
+        .value_of("nth-derivative")
+        .unwrap_or(&"0")
+        .parse::<usize>()
+        .unwrap();
+    let impulse_len = 8*sigma;
     let mu = impulse_len/2;
     let raw_impulse = (0..impulse_len).map(|x| normal_pdf(mu as f64, sigma as f64, x as f64)).collect::<Vec<f64>>();
     let impulse_weight: f64 = raw_impulse.iter().sum();
     let impulse = raw_impulse.iter().map(|x| x / impulse_weight).collect::<Vec<f64>>();
+
     // println!("{}", impulse.iter().map(|x| format!("{}", x)).collect::<Vec<String>>().join(" "));
 
     let filename = matches.value_of("INPUT").unwrap();
     for_each_input_vector(filename, |name, v| {
 
-        let res = convolve(&v, &impulse, 1.0);
+        let res = if nth_derivative == 0 {
+            convolve(&v, &impulse, 1.0)
+        } else {
+            let mut q = convolve(&v, &impulse, 1.0);
+            for _i in 0..nth_derivative {
+                q = differentiate(&q);
+            }
+            q
+        };
         //let res = &_res[impulse_len-1.._res.len()-impulse_len];
         res.iter().enumerate().for_each(|(i, x)| println!("{}\t{}\t{}\t{}", name, sigma, (i as i64 - (impulse_len/2) as i64), x));
-
     });
 }
